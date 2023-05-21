3736a05bf75510491ecf058ab871463357b6a178
Sezal Saundarya <87928300+Sezalsaundarya11@users.noreply.github.com>
2023-05-21 17:50:16 +0530

    Time: 22 ms (27.19%), Space: 8.5 MB (26.13%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int total=0;
+        map<char,int> mp={{'I', 1},{'V',5},{'X',10},{'L',50},{'C',100}, {'D',500},{'M',1000}};
+        
+        for(int i=0; i<s.length(); i++){
+            if(mp[s[i]]>=mp[s[i+1]]){
+                total=total+mp[s[i]];
+            }
+            else{
+                total-=mp[s[i]];
+            }
+        }
+        return total;
+        
+    }
+};
